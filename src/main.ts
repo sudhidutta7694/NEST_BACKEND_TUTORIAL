9351570beabd45e5b00e354ac26ef023cb6ba5d9
sudhidutta7694 <sudhi.dutta7694@gmail.com>
2024-07-02 02:27:15 +0530

    Database and Employees Service Added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import allowedOrigins from 'config/allowedOrigins';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin : (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        return callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    optionsSuccessStatus: 200
+  });
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
